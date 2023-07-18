c59d063fc4c8227cc07c1ab9382fd3179edbeb80
aniket191103 <137919616+aniket191103@users.noreply.github.com>
2023-07-18 13:45:04 +0530

    Add files via upload

---
+++ jadoo.js
@@ -0,0 +1,11 @@
+function ani()
+{
+    let  btn=document.getElementById('rm');
+    btn.style
+
+}
+$(document).ready(function() {
+    $('#home').click(function() {
+        $('con2').css('background-image', "url('https://www.google.com/imgres?imgurl=https%3A%2F%2Fimages.unsplash.com%2Fphoto-1540829917886-91ab031b1764%3Fixlib%3Drb-4.0.3%26ixid%3DM3wxMjA3fDB8MHxzZWFyY2h8Mnx8Y29tcHV0ZXIlMjBwcm9jZXNzb3J8ZW58MHx8MHx8fDA%253D%26w%3D1000%26q%3D80&tbnid=3BakriKxJWB-aM&vet=12ahUKEwikgZu9yZeAAxU_rGMGHValCWAQMygJegUIARD7AQ..i&imgrefurl=https%3A%2F%2Funsplash.com%2Fs%2Fphotos%2Fcomputer-processor&docid=Bjm8Y15lhxpiJM&w=1000&h=667&q=processor&ved=2ahUKEwikgZu9yZeAAxU_rGMGHValCWAQMygJegUIARD7AQ')");
+    });
+});
